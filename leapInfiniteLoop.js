1435e7f5655c82ee51da15e004dba91e1bca6e62
jillianneff <jilliannneff@gmail.com>
2020-09-05 15:22:40 -0400

    deliverable 1, step 59

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,9 +4,14 @@ let y = (window.innerHeight/2);
 let randomX = 0;
 let randomY = 0;
 Leap.loop(controllerOptions, function(frame){
-    clear();
-    randomX = Math.floor(Math.random()*3) - 1;
-    randomY = Math.floor(Math.random()*3) - 1;
-    circle(x+randomX,y+randomY,50);
+    // clear();
+    // randomX = Math.floor(Math.random()*3) - 1;
+    // randomY = Math.floor(Math.random()*3) - 1;
+    // circle(x+randomX,y+randomY,50);
+    if (frame.hands.length == 1){
+        let hand = frame.hands[0];
+        let fingers = hand.fingers;
+        console.log(fingers);
+    }
 }
 );
