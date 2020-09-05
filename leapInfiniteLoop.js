fbf9a88d351a4f2d402806186bca63827d97c591
jillianneff <jilliannneff@gmail.com>
2020-09-05 14:47:18 -0400

    deliverable 1, step 51

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,12 +1,12 @@
 let controllerOptions = {};
-let i = 0;
 let x = (window.innerWidth/2);
 let y = (window.innerHeight/2);
-let randomValue = 0;
+let randomX = 0;
+let randomY = 0;
 Leap.loop(controllerOptions, function(frame){
-    randomValue = Math.floor(Math.random()*3) - 1;
-    circle(x+randomValue,y,50);
-    console.log(i);
-    i++;
+    clear();
+    randomX = Math.floor(Math.random()*3) - 1;
+    randomY = Math.floor(Math.random()*3) - 1;
+    circle(x+randomX,y+randomY,50);
 }
 );
