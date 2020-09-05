3bbd3556995664962e78acb206457546550206b6
jillianneff <jilliannneff@gmail.com>
2020-09-05 14:44:24 -0400

    deliverable 1, step 49

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,6 +1,11 @@
-var controllerOptions = {};
-var i = 0;
+let controllerOptions = {};
+let i = 0;
+let x = (window.innerWidth/2);
+let y = (window.innerHeight/2);
+let randomValue = 0;
 Leap.loop(controllerOptions, function(frame){
+    randomValue = Math.floor(Math.random()*3) - 1;
+    circle(x+randomValue,y,50);
     console.log(i);
     i++;
 }
