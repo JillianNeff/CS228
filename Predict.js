13907f4a29c59b78edc74c75a53dbb30cab064a7
jillianneff <jilliannneff@gmail.com>
2020-09-28 18:53:52 -0400

    Deliverable 4 step 9

--- Predict.js
+++ Predict.js
@@ -1,4 +1,17 @@
+let trainingCompleted = false;
 
 function draw(){
     clear();
+    if(!trainingCompleted)
+        Train();
+    Test();
+}
+
+function Train(){
+    console.log("I am being trained");
+    trainingCompleted = true;
+}
+
+function Test(){
+    console.log("I am being tested");
 }
