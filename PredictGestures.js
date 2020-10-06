e742afa221c06f8550e305a0cb279dfd8e365734
jillianneff <jilliannneff@gmail.com>
2020-10-05 21:46:20 -0400

    Deliverable 5 step 48

--- PredictGestures.js
+++ PredictGestures.js
@@ -15,8 +15,11 @@ function Train(){
     for(let i = 0; i < train0.shape[3]; i++){
         let features = train0.pick(null,null,null,i);
         features = features.reshape(120);
-        console.log(features.toString());
         knnClassifier.addExample(features.tolist(), 0);
+
+        let features1 = train1.pick(null, null, null, i);
+        features1 = features1.reshape(120);
+        knnClassifier.addExample(features1.tolist(), 1);
     }
     trainingCompleted = true;
 }
@@ -24,7 +27,7 @@ function Train(){
 function Test(){
     for( let i = 0; i < test.shape[3]; i++){
         let features = test.pick(null,null,null,i);
-        features = features.reshape(120, 1);
+        features = features.reshape(120);
         let predictedLabel = knnClassifier.classify(features.tolist(), GotResults);
     }
 }
